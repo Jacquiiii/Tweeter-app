49f8fdcb496f11d18795e8f775e316cf06b69149
Jacquiiii <jacqui.koroll@telus.com>
2022-12-06 18:22:48 -0800

    add character count function on composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  console.log('testing');
+  $( "#tweet-text" ).on( "input", function() {
+
+    const characterCounter = $(this).val().length;
+    const counterElement = $(this).parent().siblings( ".tweet-footer" ).children( ".counter" );
+    const count = 140 - characterCounter;
+
+      if (count < 0) {
+        $(counterElement).css( "color", "red" ).text(count);
+      } else {
+        counterElement.text(count);
+      }
+
+  });
 });
+
+
