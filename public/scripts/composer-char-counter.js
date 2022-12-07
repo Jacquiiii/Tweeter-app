4b700e3e35c3201421009ef7f4c1d62cbf8748e5
Jacquiiii <jacqui.koroll@telus.com>
2022-12-06 21:17:58 -0800

    add tweet-container article and sections to index.html. add tweet-container.css file for styling tweet-container.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
   $( "#tweet-text" ).on( "input", function() {
 
+    // 
     const characterCounter = $(this).val().length;
     const counterElement = $(this).parent().siblings( ".tweet-footer" ).children( ".counter" );
     const count = 140 - characterCounter;
 
-      if (count < 0) {
-        $(counterElement).css( "color", "red" ).text(count);
-      } else {
-        counterElement.text(count);
-      }
+    if (count < 0) {
+      counterElement.text(count).addClass('red');
+    } else {
+      counterElement.text(count).removeClass('red');
+    }
 
   });
 });
