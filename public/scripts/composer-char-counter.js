68d9a96fe46d597276db4c7084545df663f3fb8c
Jacquiiii <jacqui.koroll@telus.com>
2022-12-06 21:24:50 -0800

    add comments to composer-char-counter.js to describe function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,9 @@
+// reduces starting character limit by 1 each time a character is entered on keyboard
+// colour of the count turns red when limit is exceeded
+
 $(document).ready(function() {
   $( "#tweet-text" ).on( "input", function() {
 
-    // 
     const characterCounter = $(this).val().length;
     const counterElement = $(this).parent().siblings( ".tweet-footer" ).children( ".counter" );
     const count = 140 - characterCounter;
