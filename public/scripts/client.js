05ac909a0964fb042f07769105bf4d9b438b4e89
Jacquiiii <jacqui.koroll@telus.com>
2022-12-07 22:59:10 -0800

    update comments and improve format inconsistencies. no content changes.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,10 @@
 // escapes text to prevent cross site scripting when used
-const escape = function (str) {
-  let div = document.createElement("div");
+const escape = function(str) {
+  let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
+
 
 // takes in a tweet object and returns a tweet <article> element containing the entire HTML structure of the tweet
 const createTweetElement = function(tweet) {
@@ -11,30 +12,34 @@ const createTweetElement = function(tweet) {
 
   const $tweet = `
     <article class="tweet-container">
+
       <section class="tweet-container-header">
         <div>
           <img src=${tweet.user.avatars}>
           <span>${tweet.user.name}</span>
         </div>
-        <span class="handle">${tweet.user.handle}</span>
+        <span class='handle'>${tweet.user.handle}</span>
       </section>
+
       <section class="tweet-container-body">
         <p>${escape(tweet.content.text)}</p>
       </section>
+
       <section class="tweet-container-footer">
         <span>${timeOfTweet}</span>
         <span class="tweeter-icons">
           <i class="fa-solid fa-flag"></i>
           <i class="fa-sharp fa-solid fa-retweet"></i>
           <i class="fa-solid fa-heart"></i>
-          <i class="likes"><sup>100</sup></i>
         </span>
       </section>
+
     </article>
     `;
      
   return $tweet;
 };
+
 
 // gets array of tweets from /tweets
 const loadTweets = function() {
@@ -44,6 +49,7 @@ const loadTweets = function() {
   });
 
 };
+
 
 // loops through tweets and calls createTweetElement to append new tweet to the tweets container
 const renderTweets = function(tweets) {
@@ -56,28 +62,30 @@ const renderTweets = function(tweets) {
 
 };
 
+
 // executes when dom is fully loaded
 $(document).ready(function() {
 
   // listens for submit event on tweet button
   $('form').submit(event => {
     event.preventDefault();
-    const tweetInput = $("#tweet-text").val().trim();
+    const tweetInput = $('#tweet-text').val().trim();
 
     // if tweet input is empty or exceeds character count, alert is presented
     // otherwise, form data is posted to /tweets then all /tweets data is loaded
     if (!tweetInput) {
-      $('.empty-error').slideDown("slow");
-      // alert('Tweet cannot be empty. Please enter a message to tell your friends what you\'re humming about!');
+      $('.empty-error').slideDown();
+
     } else if (tweetInput.length > 140) {
-      $('.exceeded-char-error').slideDown("slow");
-      // alert('Your tweet is too long! Please shorten your tweet so we can get to telling your friends what you\'re humming about.');
+      $('.exceeded-char-error').slideDown();
+      
     } else {
-      $('.empty-error').slideUp("slow");
-      $('.exceeded-char-error').slideUp("slow");
+      $('.empty-error').slideUp();
+      $('.exceeded-char-error').slideUp();
+      
       const formData = $('form').serialize();
       $.post('/tweets', formData, () => {
-        $("#tweet-text").val('');
+        $('#tweet-text').val('');
         $('.counter').text(140);
         loadTweets();
       });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,10 @@
 // colour of the count turns red when limit is exceeded
 
 $(document).ready(function() {
-  $( "#tweet-text" ).on( "input", function() {
+  $('#tweet-text').on('input', function() {
 
     const characterCounter = $(this).val().length;
-    const counterElement = $(this).parent().siblings( ".tweet-footer" ).children( ".counter" );
+    const counterElement = $(this).parent().siblings('.tweet-footer').children('.counter');
     const count = 140 - characterCounter;
 
     if (count < 0) {
